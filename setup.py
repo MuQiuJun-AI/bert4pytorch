caa36ab968474846af39756f3ea63e8ce8250de1
MuQiuJun-AI <m15228759020_1@163.com>
2021-09-06 16:24:40 +0800

    更新模型加载方式

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='bert4pytorch',
-    version='0.1.0',
+    version='0.1.3',
     description='an elegant bert4pytorch',
     long_description='bert4pytorch: ',
     license='Apache License 2.0',
     url='https://github.com/MuQiuJun-AI/bert4pytorch',
     author='MuQiuJun',
-    install_requires=['pytorch<=1.8.1'],
+    install_requires=['torch>1.0', 'numpy>=1.17'],
     packages=find_packages()
 )
