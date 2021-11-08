dd8220d8fe9b32226cd200e139587203764a832a
MuQiuJun-AI <m15228759020_1@163.com>
2021-11-08 12:08:45 +0800

    添加conditional layernorm

--- bert4pytorch/layers.py
+++ bert4pytorch/layers.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from torch.nn import Transformer
 import math
 
 
@@ -23,19 +22,38 @@ activations = {"gelu": gelu, "relu": F.relu, "swish": swish}
 
 
 class LayerNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-12):
-        """layernorm 层，也可使用pytorch自带的layernorm，为了后续的可扩展性，这里自行实现
+    def __init__(self, hidden_size, eps=1e-12, conditional=False):
+        """layernorm 层，这里自行实现，目的是为了兼容 conditianal layernorm，使得可以做条件文本生成、条件分类等任务
+           条件layernorm来自于苏剑林的想法，详情：https://spaces.ac.cn/archives/7124
         """
         super(LayerNorm, self).__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.bias = nn.Parameter(torch.zeros(hidden_size))
         self.eps = eps
+        self.conditional = conditional
+        if conditional:
+            # 条件layernorm, 用于条件文本生成,
+            # 这里采用全零初始化, 目的是在初始状态不干扰原来的预训练权重
+            self.dense1 = nn.Linear(2 * hidden_size, hidden_size, bias=False)
+            self.dense.weight.data.uniform_(0, 0)
+            self.dense2 = nn.Linear(2 * hidden_size, hidden_size, bias=False)
+            self.dense.weight.data.uniform_(0, 0)
 
     def forward(self, x):
-        u = x.mean(-1, keepdim=True)
-        s = (x - u).pow(2).mean(-1, keepdim=True)
-        x = (x - u) / torch.sqrt(s + self.eps)
-        return self.weight * x + self.bias
+        if self.conditional:
+            inputs = x[0]
+            cond = x[1]
+            for _ in range(len(inputs.shape) - len(cond.shape)):
+                cond = cond.unsqueeze(dim=1)
+            u = inputs.mean(-1, keepdim=True)
+            s = (inputs - u).pow(2).mean(-1, keepdim=True)
+            x = (inputs - u) / torch.sqrt(s + self.eps)
+            return (self.weight + self.dense1(cond)) * x + (self.bias + self.dense2(cond))
+        else:
+            u = x.mean(-1, keepdim=True)
+            s = (x - u).pow(2).mean(-1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            return self.weight * x + self.bias
 
 
 class MultiHeadAttentionLayer(nn.Module):
